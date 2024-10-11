669511a3514a7055481790f487bc1fbed74b81b9
mwihoti <danielmwihoti@gmail.com>
2024-10-11 23:02:36 +0300

    callback api & jobRow

--- app/page.js
+++ app/page.js
@@ -3,8 +3,23 @@
 import JobList from '@/components/JobList';
 import Hero from '@/components/Hero';
 import Jobs from '@/components/Jobs';
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  withAuth,
+} from '@workos-inc/authkit-nextjs';
 
-export default function Home() {
+export  default async function Home() {
+
+   // Retrieves the user from the session or returns `null` if no user is signed in
+   const { user } = await withAuth();
+
+   // Get the URL to redirect the user to AuthKit to sign in
+   const signInUrl = await getSignInUrl();
+ 
+   // Get the URL to redirect the user to AuthKit to sign up
+   const signUpUrl = await getSignUpUrl();
+ 
   return (
     <div>
             
--- components/JobRows.js
+++ components/JobRows.js
@@ -1,18 +1,28 @@
+import { faHeart} from "@fortawesome/free-regular-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 export default function JobRow() {
 
     return (
         <>
-        <div className="bg-white p-6 rounded-lg shadow-sm flex gap-4">
-            <div className="content-center">
+        <div className="bg-white p-4 rounded-lg shadow-sm relative flex gap-4">
+            <div className="absolute cursor-pointer top-4 right-0">
+                <FontAwesomeIcon className="size-5 text-gray-500" icon={faHeart}/>
+            </div>
+           <div className="flex grow gap-4">
+             <div className="content-center">
         <img className="size-12" 
         src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg"  alt="logo"/>
             </div>
+            <div>
             <div className="grow">
                 <div className="text-gray-500 text-sm">Spotify</div>
                 <div className="font-bold">Product Designer</div>
                 <div className="text-gray-400 text-sm">Remote &middot; New York, Us &middot; Full-time</div>
             </div>
+            </div>
+            
             <div className="content-end text-gray-500 text-xs">2 weeks ago</div>
+        </div>
         </div>
             </>
     );
