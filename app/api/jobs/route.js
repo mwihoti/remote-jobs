1b03dbf92695a7e7c38ac89e649a1481deb591cf
mwihoti <danielmwihoti@gmail.com>
2024-10-14 12:15:42 +0300

    updated api route for jobs

--- app/api/jobs/route.js
+++ app/api/jobs/route.js
@@ -1,28 +1,9 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../lib/prisma';
 import { withAuth as getUser } from '../../../lib/auth'
-import { promises as fs } from 'fs';
-import path from 'path';
+import { readJobsFile, writeJobsFile} from '../../../lib/jobsUtils'
 
-const dataFilePath = path.join(process.cwd(), 'data', 'jobs.json');
 
-async function readJobsFile() {
-    try {
-        const data = await fs.readFile(dataFilePath, 'utf-8');
-        return JSON.parse(data)
-    } catch (error) {
-        console.error('Error reading jobs file:', error);
-        return [];
-    }
-}
-
-async function writeJobsFile(jobs) {
-    try {
-        await fs.writeFile(dataFilePath, JSON.stringify(jobs, null, 2));
-    } catch (error) {
-        console.error('Error writing jos file.', error);
-    }
-}
 
 export async function GET(request) {
     const jobs = await readJobsFile();
